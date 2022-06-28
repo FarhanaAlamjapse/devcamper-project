804b271d7d941d7775f516638ddfd9877720a66f
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-06-28 22:22:41 +0600

    Added-Route-Structure

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
+//load env vars
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+
+//mouth router
+app.use("/api/v1/bootcamps", bootcamps);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV}mode on port ${PORT}`)
+);
