26fc26b72ed1f1577e462a870a0b49df06d5f9b3
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-06-30 23:45:01 +0600

    crud

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -1,18 +1,27 @@
 const Bootcamp = require("../models/Bootcamp");
 
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: "show all bootcamps",
-    Hello: req.hello,
-  });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `show single bootcamp${req.params.id}`,
-  });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 exports.createBootcamp = async (req, res, next) => {
