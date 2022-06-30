149f1a417e12dbeba6afbe3f8b2b68668261bd10
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-06-30 06:18:56 +0600

    creating-model

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 // const logger = require("./middleware/logger");
 const morgan = require("morgan");
+
+//load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
 
 //Route files
 const bootcamps = require("./routes/bootcamps");
 
-//load env vars
-dotenv.config({ path: "./config/config.env" });
 const app = express();
 
 // app.use(logger);
@@ -20,7 +25,12 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV}mode on port ${PORT}`)
 );
+
+//Handle unhandle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error:${err.message}`);
+});
