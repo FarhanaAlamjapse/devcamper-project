64d70246cc5d37a61d7a4fcb100b15e3e5ef9d93
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-07-03 19:43:25 +0600

    created-courses

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,8 +9,15 @@ const {
   getBootcampsInRadius,
 } = require("../controller/bootcamps");
 
+// Include other resource routers
+const courseRouter = require("./courses");
+
 //initialize router
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use("/:bootcampId/courses", courseRouter);
+
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
 router.route("/").get(getBootcamps).post(createBootcamp);
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,10 +21,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
 // Import into db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("data imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -35,6 +41,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("data destroyed...".red.inverse);
     process.exit();
   } catch (err) {
