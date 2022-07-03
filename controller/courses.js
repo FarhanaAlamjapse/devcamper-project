5cd307b7fe1609a6b06420a6d1f566ab69ee1b9d
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-07-04 02:42:33 +0600

    update-delete-course

--- controller/courses.js
+++ controller/courses.js
@@ -55,12 +55,55 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`),
-      400
+      404
     );
   }
   const course = await Course.create(req.body);
   res.status(200).json({
     success: true,
     data: course,
+  });
+});
+
+// @desc          update course
+// @route         PUT/api/v1/courses/:id
+// @access        Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc          delete course
+// @route         DELETE/api/v1/courses/:id
+// @access        Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+  await course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
   });
 });
