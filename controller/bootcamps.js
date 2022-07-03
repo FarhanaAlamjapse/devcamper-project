49d2b88ca5798afa67cd5b72b4b40a792a643e30
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-07-04 02:02:20 +0600

    find-singleCourse-and-addCourse

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   );
 
   // finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
 
   // select fields
   if (req.query.select) {
@@ -115,12 +115,13 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return;
     new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404);
   }
+  bootcamp.remove();
 
   res.status(200).json({
     success: true,
