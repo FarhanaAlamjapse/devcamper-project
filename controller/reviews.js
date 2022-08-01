5150d48c03480995436191eb0eaf0b4b5360ccb1
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-08-01 21:35:58 +0600

    Delete Review

--- controller/reviews.js
+++ controller/reviews.js
@@ -57,11 +57,51 @@ exports.addReview = asyncHandler(async (req, res, next) => {
       )
     );
   }
-
   const review = await Review.create(req.body);
 
   res.status(201).json({
     success: true,
     data: review,
+  });
+});
+
+// @desc          Update Review
+// @route         PUT/api/v1/reviews/:id
+// @access        Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+    );
+  }
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    validators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+// @desc          Delete Review
+// @route         DELETE/api/v1/reviews/:id
+// @access        Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+    );
+  }
+  await review.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
   });
 });
