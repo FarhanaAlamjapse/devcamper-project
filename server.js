88c08ffdeaa3239389adc273b7a6876e54544780
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-08-03 22:16:07 +0600

    XSS-protection

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 
@@ -43,6 +45,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
