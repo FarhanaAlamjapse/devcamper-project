7c7c37586e989d51b75c4c8ba43c1d47947e2f34
FarhanaAlamjapse <farhanjapz@gmail.com>
2022-08-03 23:13:33 +0600

    rateLimit-hpp-CORS

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const errorHandler = require("./middleware/error");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 
@@ -51,6 +54,19 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
